51e1ef5d2a492a8781a5a0d179a492100dd6b5e8
Evan / Fero <hi@evan.graphics>
2023-04-20 19:47:36 -0700

    what

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "MechaKeys",
-  base: "robolab-io.github.io",
   description: "The documentation for MechaKeys, the integrated typing environment.",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
