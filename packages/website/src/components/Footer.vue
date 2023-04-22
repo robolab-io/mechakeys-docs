d137dbb5b77f7befdbdae5ae49fe08a6ff078a78
Evan / Fero <hi@evan.graphics>
2023-04-22 11:59:51 -0700

    feat: basic landing

---
+++ packages/website/src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <!--  sm:flex-row-reverse sm:justify-between -->
+        <div class="flex flex-col items-center border-t border-slate-400/10 py-8">
+            <p class="mt-6 opacity-40 sm:mt-0">
+                Copyright © 2023 Robolab. All rights reserved.
+            </p>
+        </div>
+    </footer>
+</template>
---
+++ packages/website/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/components/**/*.{js,vue,ts}",
+    "./src/views/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
