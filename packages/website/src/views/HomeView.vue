6396fe1d650e8d3ed81dfc91787c16d28b908c7e
Evan / Fero <hi@evan.graphics>
2023-04-22 14:10:54 -0700

    feat: testimonial section

--- packages/website/src/views/HomeView.vue
+++ packages/website/src/views/HomeView.vue
@@ -5,7 +5,7 @@
     <Section>
       <HD>
         <template #heading>
-          Customize your PC boundlessly.
+          Customize your PC boundlessly
         </template>
         <template #description>
           MechaKeys gives you an open and unified environment to build on. Use what you need, or create your own
@@ -71,7 +71,7 @@
         <li>
           <ul role="list" class="flex flex-col gap-y-6 sm:gap-y-8">
             <li>
-              <figure class="relative rounded-2xl bg-[var(--app-base2)] p-6 shadow-2xl"><svg aria-hidden="true"
+              <figure class="relative rounded-2xl bg-[var(--app-base2)] p-6 shadow-2xl border-solid border-[1px] border-[var(--borders-base1)]"><svg aria-hidden="true"
                   width="105" height="78" class="absolute left-6 top-6 fill-[var(--app-fore2)] opacity-50">
                   <path
                     d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
@@ -90,7 +90,7 @@
             </li>
 
             <li>
-              <figure class="relative rounded-2xl bg-[var(--app-base2)] p-6 shadow-2xl"><svg aria-hidden="true"
+              <figure class="relative rounded-2xl bg-[var(--app-base2)] p-6 shadow-2xl border-solid border-[1px] border-[var(--borders-base1)]"><svg aria-hidden="true"
                   width="105" height="78" class="absolute left-6 top-6 fill-[var(--app-fore2)] opacity-50">
                   <path
                     d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
@@ -102,6 +102,90 @@
                 <figcaption class="relative mt-6 flex items-center justify-between pt-6">
                   <div>
                     <div class="font-display text-base text-slate-100">@GI58424124</div>
+                    <div class="mt-1 text-sm text-slate-300">Twitter</div>
+                  </div>
+                </figcaption>
+              </figure>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <ul role="list" class="flex flex-col gap-y-6 sm:gap-y-8">
+            <li>
+              <figure class="relative rounded-2xl bg-[var(--app-base2)] p-6 shadow-2xl border-solid border-[1px] border-[var(--borders-base1)]"><svg aria-hidden="true"
+                  width="105" height="78" class="absolute left-6 top-6 fill-[var(--app-fore2)] opacity-50">
+                  <path
+                    d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
+                  </path>
+                </svg>
+                <blockquote class="relative overflow-hidden aspect-video">
+                  <iframe class="absolute inset-0 w-full h-full" width="560" height="315" src="https://www.youtube.com/embed/5qOMPkvpo90?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </blockquote>
+                <figcaption class="relative mt-6 flex items-center justify-between pt-6">
+                  <div>
+                    <div class="font-display text-base text-slate-100">@Vend</div>
+                    <div class="mt-1 text-sm text-slate-300">YouTube</div>
+                  </div>
+                </figcaption>
+              </figure>
+            </li>
+
+            <li>
+              <figure class="relative rounded-2xl bg-[var(--app-base2)] p-6 shadow-2xl border-solid border-[1px] border-[var(--borders-base1)]"><svg aria-hidden="true"
+                  width="105" height="78" class="absolute left-6 top-6 fill-[var(--app-fore2)] opacity-50">
+                  <path
+                    d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
+                  </path>
+                </svg>
+                <blockquote class="relative overflow-hidden aspect-video">
+                  <iframe class="absolute inset-0 w-full h-full" width="560" height="315" src="https://www.youtube.com/embed/tPFySY0IEsY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </blockquote>
+                <figcaption class="relative mt-6 flex items-center justify-between pt-6">
+                  <div>
+                    <div class="font-display text-base text-slate-100">@gmpSNZ</div>
+                    <div class="mt-1 text-sm text-slate-300">YouTube</div>
+                  </div>
+                </figcaption>
+              </figure>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <ul role="list" class="flex flex-col gap-y-6 sm:gap-y-8">
+            <li>
+              <figure class="relative rounded-2xl bg-[var(--app-base2)] p-6 shadow-2xl border-solid border-[1px] border-[var(--borders-base1)]"><svg aria-hidden="true"
+                  width="105" height="78" class="absolute left-6 top-6 fill-[var(--app-fore2)] opacity-50">
+                  <path
+                    d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
+                  </path>
+                </svg>
+                <blockquote class="relative">
+                  <p class="text-lg tracking-tight text-slate-300">mechakeys was the goat before i had a mechanical kb</p>
+                </blockquote>
+                <figcaption class="relative mt-6 flex items-center justify-between pt-6">
+                  <div>
+                    <div class="font-display text-base text-slate-100">@oykvico</div>
+                    <div class="mt-1 text-sm text-slate-300">Twitter</div>
+                  </div>
+                </figcaption>
+              </figure>
+            </li>
+
+            <li>
+              <figure class="relative rounded-2xl bg-[var(--app-base2)] p-6 shadow-2xl border-solid border-[1px] border-[var(--borders-base1)]"><svg aria-hidden="true"
+                  width="105" height="78" class="absolute left-6 top-6 fill-[var(--app-fore2)] opacity-50">
+                  <path
+                    d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
+                  </path>
+                </svg>
+                <blockquote class="relative">
+                  <p class="text-lg tracking-tight text-slate-300">downloaded MechaKeys itsss sooo gooddddd</p>
+                </blockquote>
+                <figcaption class="relative mt-6 flex items-center justify-between pt-6">
+                  <div>
+                    <div class="font-display text-base text-slate-100">@MadTingSavage</div>
                     <div class="mt-1 text-sm text-slate-300">Twitter</div>
                   </div>
                 </figcaption>
