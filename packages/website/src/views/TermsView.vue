6335d4528fe42ac9ea439e89d9220f2888120480
Evan / Fero <hi@evan.graphics>
2023-05-18 13:13:39 -0700

    chore: ToS formatting

--- packages/website/src/views/TermsView.vue
+++ packages/website/src/views/TermsView.vue
@@ -1,40 +1,51 @@
 <template>
     <main class="flex-1">
-        <div class="max-w-7xl h-full mx-auto leading-7 px-8">
-            <h1 class="mb-8 text-4xl font-extrabold tracking-tight sm:text-4xl">Terms of Service</h1>
+        <div class="max-w-7xl h-full mx-auto leading-7 px-10">
+            <h1 class="mb-8 text-4xl font-extrabold tracking-tight sm:text-4xl whitespace-pre">MechaKeys Terms of Service</h1>
+            <h2 class="mb-4 text-xl font-bold tracking-tight sm:text-2xl">Effective: May 18, 2023</h2>
+            <h2 class="mb-16 text-xl font-bold tracking-tight sm:text-2xl">Last Updated: May 18, 2023</h2>
+
+            <h2 class="mb-4 text-lg font-bold tracking-tight sm:text-2xl">Your Account</h2>
             You must be a minimum of 13 years of age to use our services.
             Accounts are entirely the responsibility of the user. Thus any activity performed by that account will be
             attributed to the owner of the account, regardless of whether or not the activity was performed by you, or any
             other individual or entity that may be using the account. Robolab is not responsible nor liable for any activity
             that happens on user accounts.
             You agree that the information used to create your account on our services is accurate, current, and complete.
+            
+            <h2 class="mt-8 mb-4 text-xl font-bold tracking-tight sm:text-2xl">Limitation of liability</h2>
             Robolab is not to be held responsible or liable for any damages caused or incurred, directly or indirectly, from
             the usage or access of our services. You agree to hold Robolab harmless from and against any liabilities,
             damages, costs, and expenses.
+
+            <h2 class="mt-8 mb-4 text-xl font-bold tracking-tight sm:text-2xl">Software and services</h2>
             Robolab grants permission to use, access, and download our services we provide on our websites for personal,
             non-commercial use only. You agree to use Robolab services under a limited, irrevocable, non-transferable
             license. You agree that while you use and access our services, and while the agreement to these Terms are in
             effect, you cannot, will not attempt to, or assist anyone to
 
-            Copy, modify, adapt, distribute, sell, transfer, license, transmit, stream, publicly display, publicly use, or
-            abuse source code from any of our services
-            Use any services in a way not outlined in these Terms
-            Use any services for monetary gain, for the purposes of, or to contribute to, directly or indirectly
-            Reverse engineer, decompile, exploit, damage, or otherwise modify any of our services
-            Hack, phish, exploit, or cheat in any of our services
-            Use any services in an illegal manner
-            Steal, pirate, torrent, or otherwise download any services, or any dependencies, or any resources, or any files,
-            or any references in any of our services in any manner other than Robolab websites.
-            Modify files in any of our services while they are downloaded, including the deletion, addition, or copying of.
+            <ul class="list-disc my-4">
+                <li>Copy, modify, adapt, distribute, sell, transfer, license, transmit, stream, publicly display, publicly use, or abuse source code from any of our services.</li>
+                <li>Use any services in a way not outlined in these Terms.</li>
+                <li>Use any services for the purposes of, or to contribute to, directly or indirectly, monetary gain.</li>
+                <li>Reverse engineer, decompile, exploit, damage, or otherwise modify any of our services.</li>
+                <li>Hack, phish, exploit, or cheat in any of our services.</li>
+                <li>Use any services in an illegal manner.</li>
+                <li>Steal, pirate, torrent, or otherwise download any services, or any dependencies, or any resources, or any files, or any references in any of our services in any manner other than Robolab websites.</li>
+                <li>Modify locally downloaded files in any of our services, including the deletion, addition, or copying of.</li>
+            </ul>
 
             No licenses, grants, or rights are given to you, by implication, or otherwise under any intellectual property
             rights owned or controlled by Robolab. You may not use "MechaKeys" or "Robolab" and their respective logomarks
             as your own.
             Robolab reserves the right, at any point in time, to reject any email, username, data, or information that you
             provide to our services. Robolab reserves the right to refuse and revoke access to usage or access of our
-            services, without notice, for any reason, at any time, and for any length of time. Robolab is under no
-            obligation to compensate you for any loss, including any lost premium products purchased on our services.
-            Robolab does not provide refunds for any premium products purchased on our services, except at our sole
+            services, without notice, for any reason, at any time, and for any length of time. 
+            
+            <h2 class="mt-8 mb-4 text-xl font-bold tracking-tight sm:text-2xl">Paid services and virtual goods</h2>
+            Robolab is under no
+            liability to compensate you for any loss, including any lost premium products purchased on our services.
+            Robolab does not have liability to you for any premium products purchased on our services, except at our sole
             discretion. You agree that when purchasing a premium product, that the purchase you performed is final,
             non-refundable, non-exchangable, and non-transferable, except at our sole discretion. Robolab reserves the right
             to reverse and prevent future purchases from your account.
@@ -42,8 +53,9 @@
             capacity, you will indemnify and hold Robolab and its officers, directors, employees, and agents harmless from
             and against any claims, liabilities, damages, and costs related to your access or use of our services or your
             violation of these Terms of Service.
-            All users of our services may contact Robolab for any inquiries via e-mail. Please contact mail (dot) robolab
-            (dot) io (at) gmail.com
+
+            <h2 class="mt-8 mb-4 text-xl font-bold tracking-tight sm:text-2xl">Contact</h2>
+            All users of our services may contact Robolab for any inquiries via e-mail. Please contact mail.robolab.io@gmail.com
         </div>
     </main>
 </template>
